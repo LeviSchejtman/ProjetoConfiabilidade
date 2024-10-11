6b86ed0695cfb521287fcf0dda45c1185382f31e
LeviSchejtman <l.schejtman@edu.pucrs.br>
2024-10-11 14:18:14 -0300

    76 Commit

--- Unity/examples/foo/test/TestFoo.c
+++ Unity/examples/foo/test/TestFoo.c
@@ -16,79 +16,79 @@ TEST(Foo, VetorOrdenado)
 {
   // All of these should pass
   int a[] = {0,1,2,3,4,5,6,7,8,9};
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",COUNTING));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",COUNTING));
 }
 
 TEST(Foo, VetorInverso)
 {
   int a[] = {9,8,7,6,5,4,3,2,1,0};
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",COUNTING));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",COUNTING));
 }
 
 TEST(Foo, VetorRepetido)
 {
   int a[] = {9,8,7,6,5,5,4,3,2,1};
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",COUNTING));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",COUNTING));
 }
 
 TEST(Foo, VetorIguais)
 {
   int a[] = {5,5,5,5,5,5,5,5,5,5};
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",COUNTING));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",COUNTING));
 }
 
 TEST(Foo, VetorNegativo)
 {
   int a[] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",COUNTING));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",COUNTING));
 }
 
 TEST(Foo, VetorNegativoPositivo)
 {
   int a[] = {-1,2,-3,4,-5,6,-7,8,-9,10};
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,9,"On",COUNTING));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,10,"On",COUNTING));
 }
 
 TEST(Foo, VetorLimiteMin)
@@ -106,13 +106,13 @@ TEST(Foo, VetorLimiteMin)
 
 TEST(Foo, VetorLimiteMax)
 {
-  int a[] = {19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,3,2,1,0};
-  TEST_ASSERT_EQUAL(0, sort(a,19,"Onlogn",QUICK));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"Onlogn",MERGE));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"Onlogn",HEAP));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"On2",SELECTION));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"On2",INSERTION));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"On2",BUBBLE));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"On",RADIX));
-  TEST_ASSERT_EQUAL(0, sort(a,19,"On",COUNTING));
+  int a[] = {20,18,17,16,15,14,13,12,11,10,9,8,7,6,5,3,2,1,0};
+  TEST_ASSERT_EQUAL(0, sort(a,20,"Onlogn",QUICK));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"Onlogn",MERGE));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"Onlogn",HEAP));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"On2",SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"On2",INSERTION));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"On2",BUBBLE));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"On",RADIX));
+  TEST_ASSERT_EQUAL(0, sort(a,20,"On",COUNTING));
 }
